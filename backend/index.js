9ff5fdec78915fcf68ac7b6d19a31f25afe52474
user nameQtadaAlBalwegit config --global user.name user <qtadaahmad@gmail.com>
2023-09-24 23:03:07 +0300

    employeeLogin-Qutada

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const employeeRouter = express.Router();
 
-const { registerEmployee } = require("../controllers/employee");
+const { registerEmployee,loginRegister } = require("../controllers/employee");
 
 employeeRouter.post("/register", registerEmployee);
-
+employeeRouter.post("/login", loginRegister);
 
 module.exports = employeeRouter;
