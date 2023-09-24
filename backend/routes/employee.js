7ac0df3d630d5284a45af28ca67fc306a4a6c37c
user nameQtadaAlBalwegit config --global user.name user <qtadaahmad@gmail.com>
2023-09-25 01:47:37 +0300

    getAllEmployee&CreateEmployeCategorys

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const employeeRouter = express.Router();
 
-const { registerEmployee,loginEmployee,updateEmployeeById,deleteEmployeeById ,getAllEmployees,getEmployeeById} = require("../controllers/employee");
+const { registerEmployee,loginEmployee,updateEmployeeById,deleteEmployeeById ,getAllEmployees,getEmployeeById,CreateEmployeeCategory ,getAllCategoryes} = require("../controllers/employee");
 
+employeeRouter.get("/categoryes", getAllCategoryes);
 employeeRouter.post("/register", registerEmployee);
 employeeRouter.post("/login", loginEmployee);
 employeeRouter.put("/:id", updateEmployeeById);
 employeeRouter.delete("/:id", deleteEmployeeById);
 employeeRouter.get("/", getAllEmployees);
 employeeRouter.get("/:id", getEmployeeById);
+employeeRouter.post("/", CreateEmployeeCategory);
 module.exports = employeeRouter;
