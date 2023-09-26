862c2191b8830b4a7c07750b6e90e676f2a98181
AlAbedMohammad <mohammad.raedabed@gmail.com>
2023-09-27 02:55:11 +0300

    git all products

--- frontend/src/routers/routers.jsx
+++ frontend/src/routers/routers.jsx
@@ -1,26 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
-import Products from "../pages/Products/products/Products";
+import Products,{ productsLoader } from "../pages/Products/products/Products";
 import Main from "../layouts/Main/Main";
 import Register from "../pages/Register/Register";
-import Login from "../pages/Login/Login.Jsx";
+import Login from "../pages/Login/Login";
+// import { productsLoader } from "../service/api/ProductsAPI/Products";
 
-export const router = createBrowserRouter(
-[  {
+export const router = createBrowserRouter([
+  {
     path: "/",
     element: <Main />,
-    children: [{
-        path: "/login",
+    children: [
+      {
+        path: "login",
         element: <Login/>,
       },
       {
-        path: "/register",
+        path: "register",
         element: <Register />,
       },
       {
-        path: "/products",
+        path: "products",
         element: <Products />,
-      }],
+        loader:productsLoader
+      },
+    ],
   },
-  ]
-
-);
+]);
