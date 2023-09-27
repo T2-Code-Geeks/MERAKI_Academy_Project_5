29cb132c03f4ff05dd80d5cf11d00c4c92186cdb
AbdallahAlJamal <abdullah.r.aljamal@gmail.com>
2023-09-27 23:11:25 +0300

    test

---
+++ frontend/src/pages/Register/Register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RegisterEmployee from "../../components/Register/RegisterEmployee";
+const Register = () => {
+    return (
+        <div>
+            Register
+            <RegisterEmployee />
+        </div>
+    );
+};
+
+export default Register;
