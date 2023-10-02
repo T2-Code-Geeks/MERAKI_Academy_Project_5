f37a5f62d0e10cd8d514cb067ab8fbf9ca139c5a
AlAbedMohammad <mohammad.raedabed@gmail.com>
2023-10-02 22:29:02 +0300

    cart slice

---
+++ frontend/src/service/redux/reducers/cart.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cart = createSlice({
+  name: "cart",
+  initialState: {
+    cart: [],
+  },
+  reducers: {
+    setCart: (state, action) => {
+      state.cart = action.payload;
+    },
+    addToCart: (state, action) => {
+      state.cart.push(action.payload);
+    },
+    updateItemById: (state, action) => {
+      const updatedCart = action.payload;
+      state.cart = state.cart.map((item) =>
+        item.id === updatedCart.id ? updatedCart : item
+      );
+    },
+    deleteItemById: (state, action) => {
+      state.cart = state.cart.filter((item) => item.id !== action.payload);
+    },
+  },
+});
+export const {
+    setCart,
+    addToCart,
+    updateItemById,
+    deleteItemById,
+} = cart.actions;
+export default cart.reducer;
--- frontend/src/service/redux/store.jsx
+++ frontend/src/service/redux/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducers/authSlice";
 import productReducer from "./reducers/productSlice";
+import cartReducer from "./reducers/cart";
 
 export default configureStore({
     reducer: {
         auth: authReducer,
-        products:productReducer
+        products:productReducer,
+        cart:cartReducer
     },
 });
