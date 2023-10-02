d74067ced2563059c089bd0cb83363cce46ed328
AlAbedMohammad <mohammad.raedabed@gmail.com>
2023-10-02 21:47:48 +0300

    side bar css

--- frontend/src/layouts/Admin/AdminLayOut.jsx
+++ frontend/src/layouts/Admin/AdminLayOut.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Outlet } from "react-router";
 import NavBar from "../../components/Admin/NavBarAdmin/NavBar";
 import Footer from "../../components/Admin/FooterAdmin/Footer";
-
+import SideBar from "../../components/Admin/SideBarAdmin/SideBar";
+import "./AdminLayOut.css"
 const AdminLayOut = () => {
     return (
         <div>
             <header>
                 <NavBar />
             </header>
-            <main>
-                <Outlet />
+            <main id="main">
+                <SideBar id="sidebar"/>
+                <Outlet  id="outlet"/>
             </main>
             <Footer />
         </div>
