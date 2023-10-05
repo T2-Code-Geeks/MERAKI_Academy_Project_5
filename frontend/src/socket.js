cf946659f4041051611b8f43bd405ad25b368c33
AlAbedMohammad <mohammad.raedabed@gmail.com>
2023-10-05 18:22:37 +0300

    socketio connect react

---
+++ frontend/src/socket.js
@@ -0,0 +1,12 @@
+import ip from "socket.io-client";
+
+const socketInt = ({ user_id, token }) => {
+ return io("http://localhost:5000", {
+    extraHeaders: {
+      user_id,
+      token,
+    },
+    
+  });
+};
+ export default socketInt
