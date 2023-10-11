602e89bac37134dc186464758902faa3e7d27739
AlAbedMohammad <mohammad.raedabed@gmail.com>
2023-10-11 03:27:41 +0300

    procut page

--- frontend/src/layouts/Main/Main.jsx
+++ frontend/src/layouts/Main/Main.jsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import { Outlet } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 
 const Main = () => {
+    const [isDarkMode, setIsDarkMode] = useState(true)
   return (
     <div>
       <header>
         <NavBar />
       </header>
       <main>
-        <section class="bg-white dark:bg-gray-700">
-          <Outlet />
+      <section className={`bg-${isDarkMode ? 'gray-700' : 'white'} dark:bg-gray-700`}>    
+            <Outlet isDarkMode={isDarkMode}/>
         </section>
       </main>
       <Footer />
