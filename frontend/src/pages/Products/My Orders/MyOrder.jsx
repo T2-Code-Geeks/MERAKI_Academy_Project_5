6864743e031b681e3f85f25abd264a15aeba7d63
AbdallahAlJamal <abdullah.r.aljamal@gmail.com>
2023-10-14 22:26:38 +0300

    working on my orders page

---
+++ frontend/src/pages/Products/My Orders/MyOrder.jsx
@@ -0,0 +1,90 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+const MyOrder = () => {
+    const { tokenUser } = useSelector((state) => state.auth);
+    const [result, setResult] = useState("");
+    useEffect(() => {
+        getUserOrders();
+    }, []);
+    const getUserOrders = async () => {
+        try {
+            const result = await axios.get(
+                "http://localhost:5000/users/orders/all",
+                {
+                    headers: { Authorization: `Bearer ${tokenUser}` },
+                }
+            );
+            if (result.data.success) {
+                setResult(result.data.result);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+    return (
+        <div className="container p-2 mx-auto sm:p-4 dark:text-gray-100">
+            <h2 className="mb-4 text-2xl font-semibold leadi">Invoices</h2>
+            <div className="overflow-x-auto">
+                <table className="min-w-full text-xs">
+                    <colgroup>
+                        <col />
+                        <col />
+                        <col />
+                        <col />
+                        <col />
+                        <col className="w-24" />
+                    </colgroup>
+                    <thead className="dark:bg-gray-700">
+                        <tr className="text-left">
+                            <th className="p-3">Order Id</th>
+                            <th className="p-3">Items</th>
+                            <th className="p-3">Shipping Date</th>
+                            <th className="p-3 text-right">Total Amount</th>
+                            <th className="p-3">Status</th>
+                        </tr>
+                    </thead>
+                    
+                        {
+                            result && result.map((order) => {
+                                return (
+                                    <tbody key={order.id}>
+                                        <tr className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
+                                            <td className="p-3">
+                                                <p>97412378923</p>
+                                            </td>
+                                            <td className="p-3">
+                                                <p>14 Jan 2022</p>
+                                                <p className="dark:text-gray-400">
+                                                    Friday
+                                                </p>
+                                            </td>
+                                            <td className="p-3">
+                                                <p>01 Feb 2022</p>
+                                                <p className="dark:text-gray-400">
+                                                    Tuesday
+                                                </p>
+                                            </td>
+                                            <td className="p-3 text-right">
+                                                <p>$15,792</p>
+                                            </td>
+                                            <td className="p-3 text-right">
+                                                <span className="px-3 py-1 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900">
+                                                    <span>Pending</span>
+                                                </span>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                );
+                            })
+                        }
+                        
+                    
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default MyOrder;
